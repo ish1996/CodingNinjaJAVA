9206fc7c0a6221f87ee78723270e2d7e310d8923
Ishwarya Rajkumar <ishwaryarajkumar@gmail.com>
2023-04-13 13:56:13 +0530

    recursion

---
+++ src/Recursion/fibonacciSeries.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+public class fibonacciSeries {
+    public static void main(String[] args)
+    {
+        System.out.println(fibo(6));
+    }
+
+    private static int fibo(int i) {
+        if(i==0)
+        {
+            return 0;
+        }
+        int oneless = fibo(i-1);
+        int output = i * oneless;
+        return output;
+    }
+
+}
